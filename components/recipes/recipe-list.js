e57c90cdc3439261c3b5faa3fda84ce06c37aa83
Joshua Richter <joshuawiidrichter@gmail.com>
2023-10-17 12:03:57 +0200

    Added functionality to view individual recipes / Added basic styling

--- components/recipes/recipe-list.js
+++ components/recipes/recipe-list.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import classes from "./recipe-list.module.css"; 
-import ShowMore from "../icons&Buttons/show-more";
+import ViewRecipeBtn from "./view-recipe-bt";
+import Link from "next/link"
 
 function RecipeList() {
   const [data, setData] = useState([]);
@@ -52,13 +53,13 @@ function RecipeList() {
             <div className={classes.cardContent}>
               <h2 className={classes.cardTitle}>{item.title}</h2>
               <p className={classes.cardCategory}>Category: {item.category}</p>
-              
+              <Link href={`/recipe/${item._id}`}>
+                    <ViewRecipeBtn />
+              </Link>
             </div>
           </div>
         ))}
       </div>
-      <br></br>
-      <ShowMore/>
     </div>
   );
 }
--- pages/api/mongodb.js
+++ pages/api/mongodb.js
@@ -42,3 +42,11 @@ async function fetchRecipes(req, res) {
 }
 
 export default fetchRecipes;
+
+export async function getEventById(id) {
+  await client.connect();
+  const db =  client.db("devdb")
+  
+  const collection = db.collection("recipes");
+  return collection.findOne({ _id:id});
+}
