a5b3c2a33fe013d8b5de566065a60dfd68aa63db
Fabphokie <mmaphokengsenne@gmail.com>
2023-11-09 14:10:21 +0200

    carousel updates

--- components/home-page/carousel.js
+++ components/home-page/carousel.js
@@ -1,28 +1,62 @@
-import React from 'react';
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
-
-const CustomArrow = ({ type, onClick }) => {
-  return (
-    <button onClick={onClick} className="carousel-arrow">
-      {type === 'PREV' ? <FaChevronLeft /> : <FaChevronRight />}
-    </button>
-  );
-};
+import React, { useState, useEffect, useRef } from 'react';
+import styles from '../recipes/recipe-list.module.css';
 
 const MyCarousel = ({ images }) => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const slidesRef = useRef([]);
+
+  useEffect(() => {
+    showSlide(slideIndex);
+  }, [slideIndex]);
+
+  const showSlide = (n) => {
+    const slides = slidesRef.current;
+    if (!slides || slides.length === 0) return;
+    if (n >= slides.length) {
+      setSlideIndex(0);
+    } else if (n < 0) {
+      setSlideIndex(slides.length - 1);
+    } else {
+      for (let i = 0; i < slides.length; i++) {
+        if (slides[i]) {
+          slides[i].style.display = 'none';
+        }
+      }
+      if (slides[slideIndex]) {
+        slides[slideIndex].style.display = 'block';
+      }
+    }
+  };
+
+  const handlePaginationClick = (index) => {
+    setSlideIndex(index);
+  };
+
   return (
-    <Carousel className="carouselWrapper" renderArrow={CustomArrow}>
-      {images.map((image, index) => (
-        <img
-          className="carousel-item img "
-          key={index}
-          src={image}
-          alt={`Recipe Image ${index + 1}`}
-          width={200}
-          height={200}
-        />
-      ))}
-    </Carousel>
+    <div className={styles.carousel}>
+      <div className={styles.slides}>
+        {images.map((image, index) => (
+          <img
+            ref={(el) => (slidesRef.current[index] = el)}
+            key={index}
+            src={image}
+            alt={`Recipe Image ${index + 1}`}
+            width={200}
+            height={200}
+            style={{ display: index === slideIndex ? 'block' : 'none' }}
+          />
+        ))}
+      </div>
+      <div className={styles.pagination}>
+        {images.map((_, index) => (
+          <span
+            key={index}
+            className={index === slideIndex ? styles.active : styles.dot}
+            onClick={() => handlePaginationClick(index)}
+          ></span>
+        ))}
+      </div>
+    </div>
   );
 };
 
