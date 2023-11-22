237cad5f5e9d7eb8748d89c9834581edd87c4a94
Fabphokie <mmaphokengsenne@gmail.com>
2023-11-22 14:51:54 +0200

    Updates

--- pages/recipe/index.js
+++ pages/recipe/index.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import { Fragment } from 'react';
-
-
-
-
-export default function AllRecipes(props) {
-  // console.log(props.data);
- 
 import RecipeList from '@/components/recipes/recipe-list';
 import ArrowIpIcon from '@/components/icons&Buttons/arrow-up-icon';
 import { run } from '../../database/recipesModule';
