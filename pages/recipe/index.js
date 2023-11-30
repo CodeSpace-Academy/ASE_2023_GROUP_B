6b03b2accf23455e1c5595e2a9ad0d4cfceff88f
HopeAobakwe <125873006+HopeAobakwe@users.noreply.github.com>
2023-11-30 15:59:41 +0200

    working fully fuctional code

--- pages/recipe/index.js
+++ pages/recipe/index.js
@@ -1,27 +1,72 @@
 import React from 'react';
-import { Fragment } from 'react';
 import RecipeList from '@/components/recipes/recipe-list';
 import ArrowIpIcon from '@/components/icons&Buttons/arrow-up-icon';
 import { getRecipes } from '../../database/recipesModule';
+import { useState, useEffect } from "react";
+import Sort from "../../components/recipes/sort"; // Adjust the import path accordingly
 
-export default function AllRecipes(props) {
+function AllRecipes({ data }) {
+  const [sortedData, setSortedData] = useState(data);
+  const [sortOrder, setSortOrder] = useState("default");
+
+  const handleSort = (order) => {
+    setSortOrder(order);
+  };
+
+  useEffect(() => {
+    sortData();
+  }, [sortOrder, data]);
+
+  const sortData = () => {
+    let sorted = [...data];
+
+    switch (sortOrder) {
+      case "newest":
+        sorted.sort((a, b) => new Date(b.published) - new Date(a.published));
+        break;
+      case "cook-asc":
+        sorted.sort((a, b) => a.cook - b.cook);
+        break;
+      case "cook-desc":
+        sorted.sort((a, b) => b.cook - a.cook);
+        break;
+      case "prep-asc":
+        sorted.sort((a, b) => a.prep - b.prep);
+        break;
+      case "prep-desc":
+        sorted.sort((a, b) => b.prep - a.prep);
+        break;
+      case "steps-asc":
+        sorted.sort((a, b) => a.instructions.length - b.instructions.length);
+        break;
+      case "steps-desc":
+        sorted.sort((a, b) => b.instructions.length - a.instructions.length);
+        break;
+      default:
+        // Default sorting logic if needed
+        break;
+    }
+
+    setSortedData(sorted);
+  };
 
   return (
-    <Fragment>
-      
-      <RecipeList data={props.data} />
+    <div>
+      <Sort onSort={handleSort} />
+      <RecipeList data={sortedData} />
       <ArrowIpIcon />
-    </Fragment>
+    </div>
   );
 }
 
 export async function getServerSideProps() {
-  const data = await getRecipes(1);
+  const data = await getRecipes(1); // Replace with your data fetching logic
 
   return {
     props: {
       data: data,
-      
     },
   };
 }
+
+export default AllRecipes;
