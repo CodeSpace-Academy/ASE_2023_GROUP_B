80c2defdbb8b4453416bd5afce09007965d86fe2
MinkyVuyi <dialevuyi@gmail.com>
2023-12-06 19:08:06 +0200

    main-nav to header

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,10 +1,43 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import classes from './header.module.css';
-import { FaUser, FaEnvelope } from 'react-icons/fa';
-import { HiOutlineHeart } from 'react-icons/hi';
+import React, { useState, useEffect } from 'react';
+import Link from "next/link";
+import Image from "next/image";
+import classes from "./header.module.css";
+import { FaUser, FaEnvelope } from "react-icons/fa";
+import { HiOutlineHeart } from "react-icons/hi";
 
 const Header = () => {
+  const [favoritesCount, setFavoritesCount] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    // Fetch the count of favorites when the component mounts
+    const fetchFavoritesCount = async () => {
+      try {
+        const response = await fetch('/api/backend/favoritesCount');
+        const data = await response.json();
+        setFavoritesCount(data.count);
+      } catch (error) {
+        console.error('Error fetching favorites count:', error);
+      }
+    };
+
+    fetchFavoritesCount();
+  }, []);
+
+  const updateFavoritesCount = async () => {
+    try {
+      const response = await fetch('/api/backend/favoritesCount');
+      const data = await response.json();
+      setFavoritesCount(data.count);
+    } catch (error) {
+      console.error('Error updating favorites count:', error);
+    }
+  };
+
   return (
     <header className={classes.header}>
       <div className={classes.projectIcon}>
@@ -19,42 +52,64 @@ const Header = () => {
       </div>
 
       <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link className={classes.recipes} href="/recipe" title="All Recipes">
-           All Recipes
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={classes.icon}
-              href="/recipe/AboutUs"
-              title="About us"
-            >
-              <FaUser />
-            </Link>
-          </li>
+        <div
+          className={`${classes.burgerMenu} ${
+            isOpen ? classes.burgerMenuOpen : ''
+          }`}
+          onClick={toggleMenu}
+        >
+          <div className={classes.burgerIcon}>
+            <div className={classes.bar1}></div>
+            <div className={classes.bar2}></div>
+            <div className={classes.bar3}></div>
+          </div>
 
-          <li>
-            <Link
-              className={classes.icon}
-              href="/recipe/Contact"
-              title="Contact"
-            >
-              <FaEnvelope />
-            </Link>
-          </li>
+          <div className={classes.menu}>
+            <ul>
+              <Link className={classes.recipes} href="/recipe">
+                All Recipes
+              </Link>
 
-          <li>
-            <Link
-              className={classes.icon}
-              href="/recipe/favoritePage"
-              title="Favorite"
-            >
-              <HiOutlineHeart />
-            </Link>
-          </li>
-        </ul>
+              <li>
+                <Link
+                  className={classes.icon}
+                  href="/recipe/AboutUs"
+                  title="About us"
+                >
+                  <FaUser />
+                </Link>
+              </li>
+
+              <li>
+                <Link
+                  className={classes.icon}
+                  href="/recipe/Contact"
+                  title="Contact"
+                >
+                  <FaEnvelope />
+                </Link>
+              </li>
+
+              <li>
+                {/* Display the favorite icon with the count */}
+                <Link
+                  className={classes.Favicon}
+                  href="/recipe/favoritePage"
+                  title="Favorite"
+                >
+                  <div>
+                    <HiOutlineHeart />
+                    {favoritesCount > 0 && (
+                      <span className={classes.favoritesCount}>
+                        {favoritesCount}
+                      </span>
+                    )}
+                  </div>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
     </header>
   );
